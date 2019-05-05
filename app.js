0ba1ff95ffe596e9a298ff18084eb8010e6cb890
Rahul Talreja <talrejarahul51@gmail.com>
2019-05-05 17:40:46 +0000

    mongodb atlas

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(flash());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-mongoose.connect("mongodb+srv://travelntrek:alliswell@cluster0-blofl.mongodb.net/test?retryWrites=true",  { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb+srv://travelntrek:alliswell5420@cluster0-blofl.mongodb.net/test?retryWrites=true",  { useNewUrlParser: true, useCreateIndex: true });
 // mongoose.connect("mongodb://localhost:27017/yelpcamp_db", { useNewUrlParser: true });
 // seedDB();
 
